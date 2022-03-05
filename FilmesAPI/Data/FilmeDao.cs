f6505de365a39f2bd96ab1a0b6a53fc32a2a06e2
Matheus Martins <math.fmartins42@gmail.com>
2022-03-05 16:36:03 -0300

    DI - Inversão de dependência e rafatoração.

---
+++ FilmesAPI/Data/FilmeDao.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using FilmesAPI.Dto;
+using FilmesAPI.Dto.FilmeDto;
+using FilmesAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeDao : IFilmeDao
+    {
+        private CinemaContext _context;
+        private IMapper _mapper;
+
+        public FilmeDao(CinemaContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Filme Create(CreateFilmeDto filmeDto)
+        {
+            var filme = _mapper.Map<Filme>(filmeDto);
+
+
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return filme;
+        }
+
+
+        public bool Delete(int id)
+        {
+            Filme filme = GetById(id);
+            if (filme == null)
+                return false;
+            
+            _context.Filmes.Remove(filme);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public Filme GetById(int id)
+        {
+            return _context.Filmes.Where(filme => filme.Id == id).FirstOrDefault();
+
+        }
+
+        public ReadFilmeDto GetFilmeById(int id)
+        {
+            var filme = GetById(id);
+            return _mapper.Map<ReadFilmeDto>(filme);
+        }
+
+        public IEnumerable<Filme> Read()
+        {
+            return _context.Filmes;
+        }
+
+        public void Update(int id, UpdateFilmeDto filmeDto)
+        {
+            Filme filme = GetById(id);
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ FilmesAPI/Data/IFilmeDao.cs
@@ -0,0 +1,18 @@
+﻿using FilmesAPI.Dto;
+using FilmesAPI.Dto.FilmeDto;
+using FilmesAPI.Models;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Data
+{
+    public interface IFilmeDao
+    {
+        Filme Create(CreateFilmeDto filme);
+        IEnumerable<Filme> Read();
+        Filme GetById(int id);
+        ReadFilmeDto GetFilmeById(int id);
+        void Update(int id, UpdateFilmeDto filme);
+        bool Delete(int id);
+
+    }
+}
